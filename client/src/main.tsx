2999f5eb004224f1c69d189e53e9f90bed3fc5b0
Samuel Odeh <samuelodeh37@gmail.com>
2026-07-30 14:02:59 +0100

    Initial commit: Set up Vite and Google Auth

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.tsx'
 import './index.css'
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '601384251709-ft9c1s2pjivg84ih1vr123umn9f2efu4.apps.googleusercontent.com';
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
 
 if (!clientId) {
   console.error("VITE_GOOGLE_CLIENT_ID is missing from your .env file!");
